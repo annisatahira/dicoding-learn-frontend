9935a24ed4dd60166151cda380f0ab0e9ab50661
annisatahira <annisatahira999@gmail.com>
2020-05-09 17:33:46 +0700

    add slideshow banner

--- FRONTEND WEB/Submission/assets/setup.js
+++ FRONTEND WEB/Submission/assets/setup.js
@@ -6,3 +6,21 @@ window.addEventListener("scroll", function () {
     document.querySelector("nav").style.backgroundColor = "transparent";
   }
 });
+
+//automatic slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var sliders = document.getElementsByClassName("slide");
+  for (i = 0; i < sliders.length; i++) {
+    sliders[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > sliders.length) {
+    slideIndex = 1;
+  }
+  sliders[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
+}
