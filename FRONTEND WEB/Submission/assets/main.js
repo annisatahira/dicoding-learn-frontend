65c4ca8bda68772115c8580ce144b65a6dcfb2c0
annisatahira <annisatahira999@gmail.com>
2020-05-10 22:08:50 +0700

    add toggle menu

--- FRONTEND WEB/Submission/assets/main.js
+++ FRONTEND WEB/Submission/assets/main.js
@@ -1,19 +1,30 @@
 //change nav background color when scrolling
-window.addEventListener("scroll", function () {
-  if (scrollY > 50) {
-    document.querySelector("nav").style.backgroundColor = "black";
+function myFunction(media) {
+  if (media.matches) {
+    // If media query matches
+    window.addEventListener("scroll", function () {
+      if (scrollY > 50) {
+        document.querySelector("nav").style.backgroundColor = "black";
+      } else {
+        document.querySelector("nav").style.backgroundColor = "transparent";
+      }
+    });
   } else {
-    document.querySelector("nav").style.backgroundColor = "transparent";
+    document.querySelector("nav").style.backgroundColor = "black";
   }
-});
+}
+
+var media = window.matchMedia("(min-width: 501px)");
+myFunction(media); // Call listener function at run time
+media.addListener(myFunction); // Attach listener function on state changes
 
 //automatic slideshow
-var slideIndex = 0;
-showSlides();
+let slideIndex = 0;
+slidesShow();
 
-function showSlides() {
-  var i;
-  var sliders = document.getElementsByClassName("slide");
+function slidesShow() {
+  let i;
+  let sliders = document.getElementsByClassName("slide");
   for (i = 0; i < sliders.length; i++) {
     sliders[i].style.display = "none";
   }
@@ -23,4 +34,14 @@ function showSlides() {
   }
   sliders[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 7000); // Change image every 7 seconds
+}
+
+// Toggle menu
+function toggleMenu() {
+  var mobileNav = document.getElementById("mobile-nav");
+  if (mobileNav.style.display == "block") {
+    mobileNav.style.display = "none";
+  } else {
+    mobileNav.style.display = "block";
+  }
 }
