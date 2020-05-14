d3319c1eea924f7c1dfdd1c02de62e9dcda51fb7
annisatahira <annisatahira999@gmail.com>
2020-05-14 17:22:11 +0700

    contoh Template Literals

---
+++ FRONTEND WEB/WebLanjutan/Practice/templateLiterals.js
@@ -0,0 +1,8 @@
+const user = {
+  firstName: "Annisa",
+  lastName: "Tahira",
+  age: 17,
+};
+
+console.log(`Hai apa kabar ${user.firstName} ${user.lastName}, 
+Selamat ulang tahun yang ke-${user.age}`);
