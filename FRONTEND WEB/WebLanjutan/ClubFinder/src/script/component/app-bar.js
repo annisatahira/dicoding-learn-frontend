e9b2b0f1c1ca5fc6c3d9ae8214a281e23efc9615
annisatahira <annisatahira999@gmail.com>
2020-05-14 16:17:33 +0700

    change to shadow DOM

--- FRONTEND WEB/WebLanjutan/ClubFinder/src/script/component/app-bar.js
+++ FRONTEND WEB/WebLanjutan/ClubFinder/src/script/component/app-bar.js
@@ -1,18 +1,39 @@
 //buat class yang mewarisi sifat HTMLELement
 class AppBar extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+  }
   //akan terpanggil ketika element
   //diterapkan pada DOM
   connectedCallback() {
     //jika ingin element diterapkan langsung
     this.render();
   }
-
   //untuk merender
   render() {
     //berfungsi untuk menampilkan element yg dibutuhkan
     //melalui property
     //lihat this.innerHTML
-    this.innerHTML = `<h2>Club Finder</h2>`;
+    this.shadowDOM.innerHTML = `
+    <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    :host {
+      display: block;
+      width: 100%;
+      background-color: cornflowerblue;
+      color: white;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    }
+    h2 {
+      padding: 16px;
+    }
+    </style>
+    <h2>Club Finder</h2>`;
   }
 }
 
--- FRONTEND WEB/WebLanjutan/ClubFinder/src/script/component/club-list.js
+++ FRONTEND WEB/WebLanjutan/ClubFinder/src/script/component/club-list.js
@@ -1,6 +1,11 @@
 import "./club-item.js";
 
 class ClubList extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+  }
+
   //untuk menerapkan property this._club
   set clubs(clubs) {
     this._clubs = clubs;
@@ -8,16 +13,27 @@ class ClubList extends HTMLElement {
   }
 
   renderError(messange) {
-    this.innerHTML = "";
-    this.innerHTML += `<h2 class="placeholder">${messange}</h2>`;
+    this.shadowDOM.innerHTML = `
+    <style>
+    .placeholder {
+      font-weight: lighter;
+      color: rgba(0, 0, 0, 0.5);
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    </style>
+    `;
+    this.shadowDOM.innerHTML += `<h2 class="placeholder">${messange}</h2>`;
   }
 
   render() {
-    this.innerHTML = "";
+    this.shadowDOM.innerHTML = "";
     this._clubs.forEach((club) => {
       const clubItemELement = document.createElement("club-item");
       clubItemELement.club = club;
-      this.appendChild(clubItemELement);
+      this.shadowDOM.appendChild(clubItemELement);
     });
   }
 }
