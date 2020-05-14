fe6e5ee18ae2213f05d70fe41d4ca279d4a2e77e
annisatahira <annisatahira999@gmail.com>
2020-05-14 20:57:35 +0700

    contoh spreading operator

---
+++ FRONTEND WEB/WebLanjutan/Practice/spreadingOperator.js
@@ -0,0 +1,9 @@
+const favorite = ["choco", "late", "milk"];
+const food = ["egg", "bread"];
+
+//membentangkan nilai array
+// console.log(...favorite, ...food);
+//menyatukan array
+const AllFood = [...favorite, ...food];
+
+console.log(...AllFood);
