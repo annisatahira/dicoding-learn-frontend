2e8a8d8a89c54b40b37a17bde5175f8cb8fc3177
annisatahira <annisatahira999@gmail.com>
2020-05-18 11:41:51 +0700

    menerpakan fetch pada club finder

--- FRONTEND WEB/WebLanjutan/ClubFinder/src/script/data/data-source.js
+++ FRONTEND WEB/WebLanjutan/ClubFinder/src/script/data/data-source.js
@@ -3,17 +3,19 @@ import clubs from "./clubs.js";
 class DataSource {
   //memakai static agar bisa diakses secara langsung tanpa harus membuat instance
   static searchClub(keyword) {
-    //mengembalikan nilai promise
-    return new Promise((resolve, reject) => {
-      const filteredClubs = clubs.filter((club) =>
-        club.name.toUpperCase().includes(keyword.toUpperCase())
-      );
-      if (filteredClubs.length) {
-        resolve(filteredClubs);
-      } else {
-        reject(`${keyword} is not found`);
-      }
-    });
+    return fetch(
+      `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(responseJson => {
+        if (responseJson.teams) {
+          return Promise.resolve(responseJson.teams);
+        } else {
+          return Promise.reject(`${keyword} is not found`);
+        }
+      });
   }
 }
 
