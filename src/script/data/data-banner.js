42d25f4a0c29a517d38b57b815c4b4099943afbf
annisatahira <annisatahira999@gmail.com>
2020-05-27 22:23:44 +0700

    add deleted file

---
+++ src/script/data/data-banner.js
@@ -0,0 +1,25 @@
+const banners = [
+  {
+    id: 1,
+    title: "Quick Learn",
+    bannerImage: "https://www.themealdb.com/images/category/dessert.png",
+    description:
+      "Lor but also the leap into electronic typesetting, remaining essentially unchanged. raset sheets containing Lorem Ipsum passages, and more ."
+  },
+  {
+    id: 2,
+    title: "Bismillah",
+    bannerImage: "https://www.themealdb.com/images/category/beef.png",
+    description:
+      "Lor but also the leap into electronic typesetting, remaining essentially unchanged. raset sheets containing Lorem Ipsum passages, and more ."
+  },
+  {
+    id: 2,
+    title: "Morning Coffe",
+    bannerImage: "https://www.themealdb.com/images/category/chicken.png",
+    description:
+      "Lor but also the leap into electronic typesetting, remaining essentially unchanged. raset sheets containing Lorem Ipsum passages, and more ."
+  }
+];
+
+export default banners;
