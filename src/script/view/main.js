38277c57cec3f1ab9b23e0373d0167ef8aabc226
annisatahira <annisatahira999@gmail.com>
2020-05-29 20:03:58 +0700

    fix responsive

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -24,7 +24,7 @@ const main = () => {
     categoryListElement.categories = categories;
 
     $("category-list").slick({
-      slidesToShow: 6,
+      slidesToShow: 5,
       slidesToScroll: 1,
       arrows: true,
       infinite: false,
@@ -32,7 +32,7 @@ const main = () => {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 5
+            slidesToShow: 4
           }
         },
         {
