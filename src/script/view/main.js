ddd13edbcc9178985222996fd93582bdae081c4d
annisatahira <annisatahira999@gmail.com>
2020-05-29 18:54:35 +0700

    change style banner slick

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -38,30 +38,17 @@ const main = () => {
     alert(message);
   };
 
-  let slideIndex = 0;
-
   const bannerSliderElement = document.querySelector("banner-slider");
 
   //memanggil setter banners() pada pada banner slider
   bannerSliderElement.banners = banners;
 
-  // const renderSlider = () => {
-  //   const bannerElement = document.querySelectorAll("banner-item");
-
-  //   for (let i = 0; i < bannerElement.length; i++) {
-  //     bannerElement[i].style.display = "none";
-  //   }
-  //   slideIndex++;
-  //   if (slideIndex > bannerElement.length) {
-  //     slideIndex = 1;
-  //   }
-  //   bannerElement[slideIndex - 1].style.display = "block";
-  //   setTimeout(renderSlider, 5000); // Change image every 5 seconds
-  // };
-
-  // renderSlider();
   $(".single-item").slick({
-    dots: true
+    dots: true,
+    fade: true,
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 2000
   });
   getCategoryMeal();
 };
