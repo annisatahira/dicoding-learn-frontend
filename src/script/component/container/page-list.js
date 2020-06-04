e9123330c0f3330a639352c8003913c430bb4240
annisatahira <annisatahira999@gmail.com>
2020-06-04 18:12:17 +0700

    fix page list

--- src/script/component/container/page-list.js
+++ src/script/component/container/page-list.js
@@ -1,6 +1,10 @@
 class PageList {
   constructor(value) {
     this.value = value;
+    console.log(this.value);
+    this.getDetailList();
+    this.renderDetailList();
+    this.showResponeMessage();
   }
 
   getDetailList = async () => {
@@ -9,10 +13,31 @@ class PageList {
         `https://www.themealdb.com/api/json/v1/1/filter.php?a=${this.value}`
       );
       const responseJson = await response.json();
-      console.log(responseJson.meals);
+      this.renderDetailList(responseJson.meals);
     } catch (error) {
       this.showResponseMessage(error);
     }
+  };
+
+  renderDetailList = meals => {
+    const detailListElement = document.querySelector("#detailList");
+    detailListElement.innerHTML = `<h1>${this.value} Foods</h1>`;
+
+    meals.forEach(meal => {
+      detailListElement.innerHTML += `
+      <div class="col s12 m6 l3">
+      <div class="card">
+      <div class="card-image">
+        <img src=${meal.strMealThumb}>
+        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">cooka</i></a>
+      </div>
+      <div class="card-content">
+      <span class="card-title">${meal.strMeal}</span>
+      </div>
+    </div>
+    </div>
+      `;
+    });
   };
 
   showResponeMessage = (message = "Check Your Connection") => {
--- src/script/view/listconfig.js
+++ src/script/view/listconfig.js
@@ -1,110 +1,13 @@
 import "../component/area-item.js";
-
-import { areas } from "../data/data-app.js";
-import TitleList from "../component/container/list-w-title.js";
 import PageList from "../component/container/page-list.js";
 
 const main = () => {
-  const areaList = new TitleList(
-    "#areaList",
-    "area-item",
-    areas,
-    "Area Categories"
-  );
-  areaList.renderItems();
-
-  $(function() {
-    //
-    //  get num of item and compute num of pages
-    //
-    var nItem = $("area-item").length;
-    var nPages = Math.ceil(nItem / $(".pagination").data("page-size"));
-    //
-    // save num pages as a data attribute of pagination element
-    //
-    $(".pagination").data("num-pages", nPages);
-    //
-    // Create the buttons on the fly
-    //
-    for (var i = 1; i < nPages; i++) {
-      $("<li/>")
-        .append($("<a/>", { href: "#", text: i + 1 }))
-        .insertBefore(".pagination li:has([rel]):last");
-    }
-    //
-    // handle pagination
-    //
-    $(".pagination li").on("click", function(e) {
-      //
-      // prevent default action
-      //
-      e.preventDefault();
-
-      //
-      // The clicked element is the next one......
-      //
-      var clickedElement = $(this);
-      var nextElement = clickedElement;
-
-      //
-      // ....if the clicked element is Next or Prev buttons
-      //
-      var nextPrevAnchorElement = clickedElement.find("a[rel]");
-      if (nextPrevAnchorElement.length == 1) {
-        //
-        // compute the next element
-        //
-        if (nextPrevAnchorElement.text().trim() == "Next") {
-          nextElement = $(".pagination li.active").next("li:not(:has([rel]))");
-          if (nextElement.length == 0) {
-            nextElement = $(".pagination li:not(:has([rel])):first");
-          }
-        } else {
-          nextElement = $(".pagination li.active").prev("li:not(:has([rel]))");
-          if (nextElement.length == 0) {
-            nextElement = $(".pagination li:not(:has([rel])):last");
-          }
-        }
-      }
-      //
-      // toggle active page
-      //
-      $(".pagination li.active").removeClass("active");
-      nextElement.addClass("active");
-
-      //
-      // get the number of active page
-      //
-      var currentPageNumber =
-        +nextElement
-          .find("a")
-          .text()
-          .trim() - 1;
-
-      //
-      // get the page size
-      //
-      var pageSize = +$(".pagination").data("page-size");
-      //
-      // toggle visibility
-      //
-      $("area-item:visible").toggle(false);
-      $("area-item")
-        .slice(currentPageNumber * pageSize, (currentPageNumber + 1) * pageSize)
-        .toggle(true);
-    });
-    //
-    // show the active page
-    //
-    $(".pagination li.active a").trigger("click");
-  });
-
-  const setValue = () => {
-    let value = (document.querySelector(
-      ".resultText"
-    ).innerHTML = localStorage.getItem("data"));
-    // const pageList = new PageList(value);
-    return value;
+  const setValue = async () => {
+    try {
+      let value = await localStorage.getItem("data");
+      const pageList = await new PageList(value);
+      return pageList;
+    } catch (error) {}
   };
 
   setValue();
